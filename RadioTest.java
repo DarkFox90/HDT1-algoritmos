af3385af80778e4b3f0af44c9eae16f667523a55
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-01-18 14:28:25 -0600

    Primera prueba unitaria a setPower

--- RadioTest.java
+++ RadioTest.java
@@ -1,3 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 public class RadioTest {
+
+    @Test
+    public void testPower(){
+        Radio radio = new Radio();
+        radio.setPower(true);
+        assertTrue(radio.isOn());
+        radio.setPower(false);
+        assertFalse(radio.isOn());
+    }
     
 }
